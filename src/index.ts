5efe0664b02c82e34f6c6b864a56db31cd5b74e6
b0wter <b0wter@gmx.de>
2022-02-16 23:55:18 +0100

    Fixed padding for log output

--- src/index.ts
+++ src/index.ts
@@ -24,12 +24,12 @@ function randomInt(min: number, max: number)
 function getNowAsString() : string
 {
     let date_ob = new Date();
-    let date = ("0" + date_ob.getDate()).slice(-2);
-    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-    let year = date_ob.getFullYear();
-    let hours = date_ob.getHours();
-    let minutes = date_ob.getMinutes();
-    let seconds = date_ob.getSeconds();
+    let date = ("0" + date_ob.getDate()).slice(-2).toString();
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2).toString();
+    let year = date_ob.getFullYear().toString();
+    let hours = date_ob.getHours().toString().padStart(2, '0');
+    let minutes = date_ob.getMinutes().toString().padStart(2, '0');
+    let seconds = date_ob.getSeconds().toString().padStart(2, '0');
     return(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
 }
 
